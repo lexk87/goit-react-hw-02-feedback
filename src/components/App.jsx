2369f8025246e4a04721b40976f18f798bc0741b
Alexandr Katyshev <katyshev1@gmail.com>
2023-02-28 20:49:43 +0200

    Section component done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    };
+
+    countTotalFeedback = () => {
+        return this.state.good + this.state.neutral + this.state.bad;
+    };
+
+    countPositiveFeedbackPercentage = () => {
+        return Math.round(
+            (this.state.good / this.countPositiveFeedbackPercentage()) * 100
+        );
+    };
+
+    render() {
+        const { good, neutral, bad } = this.state;
+        const total = this.countTotalFeedback();
+        const positivePercentage = this.countPositiveFeedbackPercentage();
+        const options = Object.keys(this.state);
+
+        return (
+            <>
+                <Section>
+                    <FeedbackOptions />
+                </Section>
+
+                <Section>
+                    {total > 0 ? <Statistics /> : <Notification />}
+                </Section>
+            </>
+        );
+    }
+}
---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const PageSection = styled.section``;
+
+export const Title = styled.h2``;
